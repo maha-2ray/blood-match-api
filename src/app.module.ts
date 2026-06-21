61e7c66af6c0774c60c6ca3f97128fdffa27cce3
Muhammed L. Touray <touraymuhammed2000@gmail.com>
2026-06-21 23:53:38 +0000

    fix: db connections

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,28 +9,57 @@ import { ChatModule } from './chat/chat.module';
 import { dataSourceOptions } from './db/datasource';
 import { HealthModule } from './health/health.module';
 
+
+// @Module({
+//   imports: [
+//     ConfigModule.forRoot({
+//       isGlobal: true,
+//       envFilePath: '.env',
+//     }),
+//     TypeOrmModule.forRoot(dataSourceOptions),
+//     TypeOrmModule.forRootAsync({
+//       imports: [ConfigModule],
+//       useFactory: (configService: ConfigService) => ({
+//         type: 'postgres',
+//         host: configService.get('DB_HOST'),
+//         port: configService.get('DB_PORT'),
+//         username: configService.get('DB_USERNAME'),
+//         password: configService.get('DB_PASSWORD'),
+//         database: configService.get('DB_NAME'),
+//         entities: [],
+//         synchronize: configService.get('NODE_ENV') === 'development',
+//         logging: configService.get('NODE_ENV') === 'development',
+//       }),
+//       inject: [ConfigService],
+//     }),
+//     HealthModule,
+//     AuthModule,
+//     UsersModule,
+//     DonorsModule,
+//     RequestsModule,
+//     ChatModule,
+//   ],
+// })
+// export class AppModule {}
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
     }),
-    TypeOrmModule.forRoot(dataSourceOptions),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-        entities: [],
-        synchronize: configService.get('NODE_ENV') === 'development',
-        logging: configService.get('NODE_ENV') === 'development',
-      }),
-      inject: [ConfigService],
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      autoLoadEntities: true,
+      synchronize: process.env.NODE_ENV === 'development',
+      logging: process.env.NODE_ENV === 'development',
     }),
+
     HealthModule,
     AuthModule,
     UsersModule,
